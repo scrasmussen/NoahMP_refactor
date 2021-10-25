5c87cf74ce27a4bda4104b394a27989ebcc5ffce
cenlinhe <cenlinhe@ucar.edu>
2021-10-25 10:37:47 -0600

    add error handle module

---
+++ utility/ErrorHandleModule.f90
@@ -0,0 +1,22 @@
+module ErrorHandleModule
+
+!!! define subroutines handling Noah-MP model errors
+
+  implicit none
+
+contains
+
+  subroutine ErrorHandle(status)
+
+    use netcdf
+
+    integer, intent (in) :: status
+ 
+    if(status /= nf90_noerr) then
+       print *, trim( nf90_strerror(status) )
+       stop "Stopped"
+    endif
+
+  end subroutine ErrorHandle
+
+end module ErrorHandleModule
