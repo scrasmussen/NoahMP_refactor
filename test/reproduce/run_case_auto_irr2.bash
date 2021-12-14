49dd4ec988d667df2ba446a0e5fc9ea5fa7d733e
cenlinhe <cenlinhe@ucar.edu>
2021-12-13 19:04:51 -0700

    add test scripts

---
+++ test/reproduce/run_case_auto_irr2.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+
+ outdir=waterall_irri2_refactor
+ runoff=3
+
+ mkdir -p ./results/${outdir}
+ rm -rf ./results/${outdir}/*
+
+ mkdir workshop
+ cd workshop
+
+ cp ../../../run/NoahmpTable.TBL .
+ cp ../../../run/noahmp_refac.exe .
+
+for irri in 1 2 3; do
+for irrim in 0 1 2 3 ; do
+ for soiltp  in 1 2 3 4 5 6 7 8 9 10 11 12 13 15 16 17 18 19 ; do
+    for vegetp in 12 ; do
+
+       namelist_output=namelist.input.irr${irri}.irrm${irrim}.runoff${runoff}.soil${soiltp}.vege${vegetp}
+       echo case.irr${irri}.irrm${irrim}.runoff${runoff}.soil${soiltp}.vege${vegetp}
+       cp ../namelists/${outdir}/${namelist_output} namelist.input
+       ./noahmp_refac.exe
+       mv output.nc ../results/${outdir}/output.nc.irr${irri}.irrm${irrim}.runoff${runoff}.soil${soiltp}.vege${vegetp}
+
+    done # vegetp
+ done # soiltp
+done
+done
+
+
+  cd ..
+  rm -r workshop
+
+# End of script
+ exit 0
+
