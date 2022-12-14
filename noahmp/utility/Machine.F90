547d31d354fd5389a742fc5b7e2d2e8b4665fd8e
Soren Rasmussen <s.c.rasmussen@gmail.com>
2022-12-14 11:53:51 -0700

    Changing undefined_real to equal NetCDF Fill Value for easier interpretation of output

--- noahmp/utility/Machine.F90
+++ noahmp/utility/Machine.F90
@@ -3,18 +3,22 @@ module Machine
 !!! define machine-related constants and parameters
 !!! To define real data type precision, use "-DOUBLE_PREC" in CPPFLAG in user_build_options file
 !!! By default, Noah-MP uses single precision
-
+  use netcdf, only : NF90_FILL_FLOAT, NF90_FILL_DOUBLE, NF90_FILL_INT
   implicit none
   save
   private
 
 #ifdef DOUBLE_PREC
   integer, public, parameter :: kind_noahmp = 8 ! double precision
+  real(kind=kind_noahmp), public, parameter :: undefined_real = NF90_FILL_DOUBLE
+  ! for variable initializatin
 #else
   integer, public, parameter :: kind_noahmp = 4 ! single precision
+  real(kind=kind_noahmp), public, parameter :: undefined_real = NF90_FILL_FLOAT
+  ! for variable initializatin
 #endif
 
   integer,                public, parameter :: undefined_int  = huge(1)    ! undefined integer number for variable initialization
-  real(kind=kind_noahmp), public, parameter :: undefined_real = huge(1.0)  ! undefined real number for variable initializatin
+
 
 end module Machine
